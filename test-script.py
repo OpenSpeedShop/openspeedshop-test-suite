02bb119b517382454f3cc1444f4965fc8c8c9206
patrick <snixromero@gmail.com>
2017-03-08 15:46:37 -0800

    smart configuration

--- test-script.py
+++ test-script.py
@@ -49,6 +49,172 @@ def test_obj(env, file_name):
     return test
 
 #####################################
+####Build profiles code########
+
+
+####################### Look for module init file #############
+#if not os.environ.has_key('MODULESHOME'): raise EnvironmentError('Environment variable "MODULESHOME" not found')
+
+# Search for these paths within the MODULESHOME directory (in this order)
+#search_paths = ['init/python',
+#'init/python.py',]
+
+# Stop at first path that exists
+#for sp in search_paths:
+#	init_py = os.path.join( os.environ['MODULESHOME'], sp )
+	#if os.path.exists(init_py): break
+
+#if not os.path.exists(init_py): raise IOError("EnvironmentModules python script was not found")
+
+# Execute the file
+#glo = {}
+#execfile(init_py, glo)
+#module = glo['module']
+
+def mpi_root(mpi_module):
+	mpi = module_kind(mpi_module)
+	module('purge')
+	try:
+		module(['load', mpi_module])
+	except:
+		print "error! module not found: " + mpi_module
+		exit(1)
+	#print str(glo)
+	if mpi == 'mpt':
+		return ('-DMPT_DIR', '$MPI_ROOT')
+	if mpi == 'mvapich2':
+		return ('-DMVAPICH2_DIR', '$MPIHOME')
+	if mpi == 'mpich':
+		if 'I_MPI_ROOT' in os.environ:
+			return ('-DMPICH_DIR', '$I_MPI_ROOT')
+		mpi_root = path_from_regex(r'.*impi.*')
+		return ('-DMPICH_DIR', mpi_root)
+	if mpi == 'openmpi':
+		#print 'doing some sketchy stuff'
+		if 'MPIHOME' in os.environ:
+			return ('-DOPENMPI_DIR', '$MPIHOME')
+		#openmpi dosent set a root variable so we need to figure that out manually
+		mpi_root = path_from_regex(r'.*open_?mpi.*')
+		return ('-DOPENMPI_DIR', mpi_root)
+
+def module_kind(module):
+	if module == '':
+		return 'gnu'
+	if re.match(r'.*gcc.*', module, re.M|re.I):
+		return 'gnu'
+	if re.match(r'.*pgi.*', module, re.M|re.I):
+		return 'pgi'
+	if re.match(r'.*intel.*', module, re.M|re.I):
+		if re.match(r'.*mpi.*', module, re.M|re.I):
+			return 'mpich'
+		if re.match(r'.*math.*', module, re.M|re.I):
+			return 'math-intel'
+		return 'intel'
+	if re.match(r'.*mpt.*', module, re.M|re.I):
+		return 'mpt'
+	if re.match(r'.*open_?mpi.*', module, re.M|re.I):
+		return 'openmpi'
+	if re.match(r'.*mvapich2.*', module, re.M|re.I):
+		return 'mvapich2'
+	if re.match(r'.*mvapich.*', module, re.M|re.I):
+		return 'mvapich'
+	if re.match(r'.*math.*', module, re.M|re.I):
+		return 'math'
+	return ''
+
+def path_from_regex(matchstr):
+	path = os.environ['PATH']
+	paths = path.split(':')
+	for p in paths:
+		if re.match(matchstr, p, re.M|re.I):
+			return p
+	
+
+def create_profiles(filename, mpi_modules, cc_modules, other_modules):
+	"""create the default profiles.json file"""
+
+	print "generated default environment file profiles.json"
+	print "please look for any inconsistencies in this file"
+
+	compatability_set = [ #to be expanded
+		('mpt', 'intel'),
+		('mpich', 'intel'),
+		('openmpi', 'intel'),
+		#('mvapich2', 'intel'),
+		('openmpi', 'gnu'),
+		('mpt', 'gnu'),
+		('mpich', 'gnu'),
+		#('mvapich2', 'gnu'),
+		('mpt', 'pgi'),
+		('openmpi', 'pgi')]
+		#('mvapich2', 'pgi')]
+
+
+	compatability_set3 = [ #to be expanded
+		('mpt', 'intel', 'math-intel'),
+		('mpich', 'intel', 'math-intel'),
+		('openmpi', 'gnu', 'math'),
+		('mpt', 'gnu', 'math') ]
+
+	profiles = []
+
+	for mpi_module in mpi_modules:
+		for cc_module in cc_modules:
+			try:
+				module('purge')
+				module(['load', cc_module])
+			except:
+				print "error! module not found: " + cc_module
+				exit(1)
+			if other_modules:
+				for other_module in other_modules:
+					module_key = (module_kind(mpi_module), module_kind(cc_module), module_kind(other_module))
+					if not module_key in compatability_set3:
+						continue
+					else:
+						if module_kind(mpi_module) == 'mpt':
+							mpi_command = 'mpiexec_mpt -np 8'
+						else:
+							mpi_command = 'mpiexec -np 8'
+
+						prof = { "cc": module_kind(cc_module),
+								"mpi_imp": module_kind(mpi_module),
+								"cc_module": cc_module,
+								"mpi_module": mpi_module,
+								'mpi_driver': mpi_command,
+								"cmake_flag_var": mpi_root(mpi_module)[1],
+								"mpi_cmake_flag": mpi_root(mpi_module)[0],
+								"other_modules":[other_module]}
+						profiles.append(prof)
+			else:
+				module_key = (module_kind(mpi_module), module_kind(cc_module))
+				if not module_key in compatability_set:
+					continue
+				else:
+					if module_kind(mpi_module) == 'mpt':
+						mpi_command = 'mpiexec_mpt -np 8'
+					else:
+						mpi_command = 'mpiexec -np 8'
+
+					prof = { "cc": module_kind(cc_module),
+						"mpi_imp": module_kind(mpi_module),
+						"cc_module": cc_module,
+						"mpi_module": mpi_module,
+						'mpi_driver': mpi_command,
+						"cmake_flag_var": mpi_root(mpi_module)[1],
+						"mpi_cmake_flag": mpi_root(mpi_module)[0],
+						"other_modules":[]}
+					profiles.append(prof)
+	print 'generating profiles.json based off the following modules'
+	print str(mpi_modules)
+	print str(cc_modules)
+	print str(other_modules)
+
+	pfile = open(filename,'w')
+	pfile.write(json.dumps(profiles,sort_keys=True, indent=2, separators=(',', ': ')))
+	pfile.close()
+
+###################################
 
 def run_cmake(build_dir, flags):
     """run a cmake sequence, build dir, cd, cmake, make, cleanup..."""
@@ -111,9 +277,36 @@ def build_tests(env):
 ## that will generally need to be modified.
 ## env contains information about the system, and
 ## profiles contains the compiler/mpi schemes that will be used to compile the tests
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
 
-def create_env(filename):
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def create_env(filename,openss_module,ompt_root):
     """create the default environment file"""
+    job_controller = 'raw'
+    if not which('qsub'):
+        job_controller = 'pbs'
+    if not which('msub'):
+        job_controller = 'slurm'
+
+    module('load', openss_module)
+    dynamic_cbtf = False
+    if os.environ['CBTF_MRNET_BACKEND_PATH']:
+        dynamic_cbtf = True
+    module('purge')
 
     print "generated default environment file env.json"
     print "please edit this to have correct values"
@@ -121,36 +314,36 @@ def create_env(filename):
         'test_data_dir':'test_data',
         'build_scripts_dir':'build_scripts',
         'src_dir':'src',
-        'dynamic_cbtf':False,
-        'oss_version': 'oss_offline-2.2.2',
-	'ompt_root':'/nobackupnfs2/jgalarow/krellroot_v2.2.6',
-        'job_controller':'raw',
+        'dynamic_cbtf':dynamic_cbtf,
+        'oss_version': openss_module,
+	'ompt_root': ompt_root,
+        'job_controller':job_controller,
         'acceptable_variance':10.0,
 	'max_concurrent_jobs':25,
-	'openss_module':'home4/jgalarow/privatemodules/osscbtf226',
+	'openss_module':openss_module,
 	'input_dir':'input_files' }
     envfile = open(filename,'w')
     envfile.write(json.dumps(env,sort_keys=True, indent=2, separators=(',', ': ')))
     envfile.close()
 
-def create_profiles(filename):
-    """create the default profiles.jsonn file"""
-
-    print "generated default environment file profiles.json"
-    print "please edit this to have correct values"
-
-    profiles = [{ "cc": "intel",
-	"mpi_imp": "mpt",
-	"cc_module": "comp-intel/2016.2.181",
-	"mpi_module": "mpi-sgi/mpt.2.12r26",
-        'mpi_driver':'mpiexec_mpt -np 8',
-	"cmake_flag_var": "$MPI_ROOT",
-	"mpi_cmake_flag": "-DMPT_DIR",
-	"other_modules":["math/intel_mkl_default"]}]
-
-    pfile = open(filename,'w')
-    pfile.write(json.dumps(profiles,sort_keys=True, indent=2, separators=(',', ': ')))
-    pfile.close()
+#def create_profiles(filename):
+    #"""create the default profiles.jsonn file"""
+#
+    #print "generated default environment file profiles.json"
+    #print "please edit this to have correct values"
+#
+    #profiles = [{ "cc": "intel",
+	#"mpi_imp": "mpt",
+	#"cc_module": "comp-intel/2016.2.181",
+	#"mpi_module": "mpi-sgi/mpt.2.12r26",
+        #'mpi_driver':'mpiexec_mpt -np 8',
+	#"cmake_flag_var": "$MPI_ROOT",
+	#"mpi_cmake_flag": "-DMPT_DIR",
+	#"other_modules":["math/intel_mkl_default"]}]
+#
+    #pfile = open(filename,'w')
+    #pfile.write(json.dumps(profiles,sort_keys=True, indent=2, separators=(',', ': ')))
+    #pfile.close()
 
 def read_profiles(filename):
     '''Read in profiles information from filename'''
@@ -229,11 +422,11 @@ def run_tests(env, tests, is_baseline):
     os.chdir(base_dir)
 		
 def moab_job_controller(env, tests):
-    pass #need to implement
+	print 'moab job controller not yet implemented'
 
 
 def slurm_job_controller(env, tests):
-    pass #need to implement
+	print 'slurm job controller not yet implemented'
 
 def pbs_job_controller(env, tests):
 
@@ -587,7 +780,7 @@ def main(args=None, error_func=None):
     parser.add_argument('--create-env', nargs='?', const='env.json',
         metavar='ENV_FILE', help='create a default environment file (default=env.json)')
     parser.add_argument('--create-prof', nargs='?', const='profiles.json',
-        metavar='ENV_FILE', help='create a default profiles file (default=profiles.json)')
+        metavar='ENV_FILE', help='create a default profiles file (default=profiles.json), use with --mpi-modules and --cc-modules')
 
     parser.add_argument('--create-baseline', action='store_true',
         help='create baseline for all tests')
@@ -604,7 +797,22 @@ def main(args=None, error_func=None):
     parser.add_argument('-r', nargs='?', type=str,
         help='use specific results folder in compare, default is the most recent')
     parser.add_argument('--baseline-version', nargs='?', type=str,
-        help='use the most recent baseline folder in this oss version to compare against')
+    	help='use the most recent baseline folder in this oss version to compare against')
+    parser.add_argument('--mpi-modules', nargs='+', type=str,
+		help='array of mpi modules')
+    parser.add_argument('--mpi-module', nargs='?', type=str,
+		help='single mpi module')
+    parser.add_argument('--cc-modules', nargs='+', type=str,
+		help='array of compiler modules')
+    parser.add_argument('--cc-module', nargs='?', type=str,
+		help='single compiler module. If none are provided we will assume gcc is in the path')
+    parser.add_argument('--oss-module', nargs='?', type=str,
+		help='The openspeedshop module to load, only needed with --create-env')
+    parser.add_argument('--ompt-root', nargs='?', type=str,
+		help='only needed if building with intel-threads, provide to --create-env')
+    parser.add_argument('--other-modules', nargs='+', type=str,
+		help='array of other modules that may be needed by some profiles')
+
     
 
     parser.add_argument('--clean-run', action='store_true' , help='clean run data')
@@ -637,14 +845,37 @@ def main(args=None, error_func=None):
     base_dir = os.getcwd()
 
     if args.create_env:
-        create_env(args.create_env)
+        if not args.oss_module:
+            print 'error: no --oss-module provided'
+            exit(0)
+        if not args.ompt_root:
+            create_env(args.create_env, args.oss_module, '')
+            exit(0)
+        create_env(args.create_env, args.oss_module, args.ompt_root)
         envfile = args.create_env
         exit(0)
 
     if args.create_prof:
-        create_profiles(args.create_prof)
-        p_file = args.create_prof
-        exit(0)
+		mpi_modules = []
+		cc_modules = []
+		other_modules = []
+
+		if args.mpi_modules:
+			mpi_modules.extend(args.mpi_modules)
+		if args.mpi_module:
+			mpi_modules.append(args.mpi_module)
+		if args.cc_modules:
+			cc_modules.extend(args.cc_modules)
+		if args.cc_module:
+			cc_modules.append(args.cc_module)
+		if args.other_modules:
+			other_modules.extend(args.other_modules)
+
+		if not cc_modules:
+			cc_modules.append('')
+			print ("no cc modules found, assuming gcc is in path (laptop mode)")
+		create_profiles(args.create_prof, mpi_modules, cc_modules, other_modules)
+		exit(0)
 
     if args.e:
         envfile = args.e
@@ -727,7 +958,11 @@ def main(args=None, error_func=None):
     #    compare_tests(env, tests_to_comp)
     if args.compare_tests:
         compare_tests(env, tests, args)
-            
+ 
+	
+
+
+           
     
 ########
 main()
